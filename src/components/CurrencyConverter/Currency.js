e5506ddec943702f8b756488102d9650bc436276
Kroi2010 <kirika@mail.ru>
2020-10-18 01:36:48 +0100

    Connecting API data with components

--- src/components/CurrencyConverter/Currency.js
+++ src/components/CurrencyConverter/Currency.js
@@ -1,16 +1,20 @@
 import React from 'react';
 import { getFullName } from '../../helpers/currencyName';
 
-const CurrencyInput = (currencyList, defaultCurrency) => {
+const CurrencyInput = ({ currencyList, defaultCurrency }) => {
+  const getOptions = () => {
+    return currencyList.map((el, index) => {
+      return (
+        <option value={el.currency} key={index}>
+          {getFullName(el.currency)}
+        </option>
+      );
+    });
+  };
+
   return (
     <div className="currency">
-      <select className="currency__type">
-        {currencyList.map((currency) => {
-          return (
-            <option value={currency.name}>{getFullName(currency.name)}</option>
-          );
-        })}
-      </select>
+      <select className="currency__type">{getOptions()}</select>
 
       <input type="number" className="currency__input" />
     </div>
--- src/components/CurrencyConverter/CurrencyConverter.js
+++ src/components/CurrencyConverter/CurrencyConverter.js
@@ -3,17 +3,21 @@ import CurrencyInput from './Currency';
 import { getCurrencyRates } from '../../api/currencyRateAPI';
 
 const CurrencyConverter = () => {
-  const [currencyList, setCurrencyList] = useState(null);
+  const [currencyList, setCurrencyList] = useState([]);
 
   useEffect(() => {
-    getCurrencyRates();
-  });
+    getCurrencyRates().then((data) => setCurrencyList(data));
+  }, []);
 
   return (
     <div className="currency-converter">
-      {currencyList ? <CurrencyInput currencyList={currencyList} /> : null}
+      {currencyList.length > 0 ? (
+        <CurrencyInput currencyList={currencyList} />
+      ) : null}
 
-      {currencyList ? <CurrencyInput currencyList={currencyList} /> : null}
+      {currencyList.length > 0 ? (
+        <CurrencyInput currencyList={currencyList} />
+      ) : null}
     </div>
   );
 };
